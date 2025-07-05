6696cb3dd6e90a8950e0f57d62db45ccf79c07aa
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-07-05 10:40:42 +0600

    add new management and install postgresqql

--- main/settings.py
+++ main/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event_management',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ from events.views import home
 # this is the main url file for the project where all the url patterns are defined
 urlpatterns = [
     path('admin/', admin.site.urls), # admin panel
-    path('',home), # home page
+    path('',home), # home page   
     path('events/',include('events.urls')) # including the url patterns from the events app
 ]
