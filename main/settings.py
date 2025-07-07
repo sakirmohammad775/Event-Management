3f91af02b000715d96a8ee5dfba7e11e31426ef5
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-07-08 00:53:37 +0600

    ass eventDetails

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-m#1(#cug-*)r(4*-=_zdpg(o258_bfc*ndu!fj81*=rif$6n-2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS=['https://*.onrender.com','http://127.0.0.1:8000']
 
 
 # Application definition
